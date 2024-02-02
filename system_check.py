f647399302a524ca44deae89e7a8955b4a713c1e
Yousif <145899994+Yousifsiddig1@users.noreply.github.com>
2024-02-02 02:05:30 +0200

    Update system_check.py

--- system_check.py
+++ system_check.py
@@ -4,15 +4,21 @@ import psutil
 
 def cpu_h():
     cpu_use = psutil.cpu_percent(interval=1)
-    return cpu_use < 75  # return True if the CPU usage is less than 75%
+    return cpu_use < 75  # Return True if the CPU usage is less than 75%
 
 def disk_u(disk):
     du = shutil.disk_usage(disk)
     free = du.free / du.total * 100
-    return free > 20  # return True if the free space is more than 20%
+    return free > 20  # Return True if the free space is more than 20%
 
 if not cpu_h() or not disk_u('/'):
     print('Error!')
 else:
     print("Everything is okay")
+
+
 #
+# Instructions for running the script in a Unix-like environment:
+# 1. Make the script executable by running: chmod +x system_check.py
+# 2. Execute the script using: ./system_check.py
+# Note: The first line in the script (shebang) is important for the executable to work.
